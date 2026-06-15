7db11a6c47436ad9d5f79098e39a17b1b9bb797d
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-15 23:07:02 +0530

    iska bhi same validator and authroise add kar diya hai

--- server/src/modules/private/score/score.routes.js
+++ server/src/modules/private/score/score.routes.js
@@ -1,12 +1,20 @@
 import express from "express";
 import ScoreController from "./score.controller.js";
+
 import { validateRequest } from "../../shared/middleware/validateRequest.js";
+
+import {
+  authenticate,
+  authorize,
+} from "../../shared/middleware/auth.middleware.js";
+
 import {
   createScoreSchema,
   updateScoreSchema,
   matchParamSchema,
   scoreParamSchema,
 } from "./dto/score.dto.js";
+
 class ScoreRoute {
   constructor(scoreController = new ScoreController()) {
     this.router = express.Router();
@@ -14,22 +22,30 @@ class ScoreRoute {
 
     this.registerRoutes();
   }
+
   registerRoutes() {
     // Create score
     this.router.post(
       "/",
+      authenticate,
+      authorize("SUPER_ADMIN", "ADMIN", "SCORER"),
       validateRequest(createScoreSchema),
       this.scoreController.createScore,
     );
+
     // Update score
     this.router.patch(
       "/:id",
+      authenticate,
+      authorize("SUPER_ADMIN", "ADMIN", "SCORER"),
       validateRequest(updateScoreSchema),
       this.scoreController.updateScore,
     );
+
     // Get scores by match
     this.router.get(
       "/match/:matchId",
+      authenticate,
       validateRequest(matchParamSchema),
       this.scoreController.getScoresByMatch,
     );
@@ -37,10 +53,13 @@ class ScoreRoute {
     // Delete score
     this.router.delete(
       "/:id",
+      authenticate,
+      authorize("SUPER_ADMIN", "ADMIN"),
       validateRequest(scoreParamSchema),
       this.scoreController.deleteScore,
     );
   }
+
   getRouter() {
     return this.router;
   }
